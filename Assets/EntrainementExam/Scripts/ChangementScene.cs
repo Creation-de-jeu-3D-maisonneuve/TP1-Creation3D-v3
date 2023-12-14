fa6531e8a6f2f3de1b76786be0077b0cac773d55
Celian136 <pianosel173@gmail.com>
2023-12-14 13:45:35 -0500

    etape1-testExamEntrainement.

---
+++ Assets/EntrainementExam/Scripts/ChangementScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangementScene : MonoBehaviour
+{
+     
+
+    public void CommencerLeJeu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
